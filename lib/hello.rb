88f38bcf2add092722d50f3a88304a2ae5eba2d9
Jake Zappin <jake.zappin@gmail.com>
2016-09-24 13:03:46 +0000

    Done.

--- lib/hello.rb
+++ lib/hello.rb
@@ -1,6 +1,14 @@
-def hello_t
-
+def hello_t(array)
+  if block_given?
+    index = 0
+    while index < array.length
+      yield array[index]
+      index += 1
+    end
+  else
+    puts "'Ey! You forgot-a the block-a!"
+  end
+  return array
 end
 
 # call your method here!
-
